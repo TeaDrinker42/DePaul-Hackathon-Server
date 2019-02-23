5938750a3429f551c79b8aebd9c6c3346051511e
TeaDrinker42 <TeaDrinker42@users.noreply.github.com>
2019-02-23 16:54:55 +0000

    sendText function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+// sample data format:
+// {'host': 'Elisabeth Campbell', 'address': 'W1F 8JB', 'link': 'https://depaul-hackathon.firebaseapp.com/', 'receiver': '+447768996754'}
+
+exports.sendText = functions.https.onCall((data) => {
+    const accountSid = 'AC8443dcb57d96ae88f15b4c318abf57c5';
+    const authToken = 'eb2882c58d1661c39e60ab5324f00040';
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+        .create({from: '+441757322044', body: 'DEPAUL CHARITY UK - Your host is ' +  data.host + ". The address is: " + data.address + ". Please find more details here: " + data.link, to: data.receiver})
+        .then(message => console.log(message.sid));
+});
+
