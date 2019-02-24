8fc0d1e489acf5ba4bb0f3c19b8f6db9cf97c596
TeaDrinker42 <TeaDrinker42@users.noreply.github.com>
2019-02-24 09:25:13 +0000

    dual debugging

--- functions/index.js
+++ functions/index.js
@@ -7,10 +7,14 @@ const client = require('twilio')(accountSid, authToken);
 // sample req format:
 // {'host': 'Elisabeth Campbell', 'address': 'W1F 8JB', 'link': 'https://depaul-hackathon.firebaseapp.com/', 'receiver': '+447746731969'}
 
-exports.sendText = functions.https.onCall((req) => {
+exports.sendText = functions.https.onCall((data) => {
+
+    // console.log(req)
+    // let data = req.body
+    // console.log(data)
 
     client.messages
-        .create({from: '+441757322044', body: '\r\n \r\nDEPAUL CHARITY UK - Your host is ' +  req.host + ". \r\n \r\nThe address is: " + req.address + ".\r\n \r\nPlease find more details here: " + req.link, to: req.receiver})
+        .create({from: '+441757322044', body: '\r\n \r\nDEPAUL CHARITY UK - Your host is ' +  data.host + ". \r\n \r\nThe address is: " + data.address + ".\r\n \r\nPlease find more details here: " + data.link, to: data.receiver})
         .then(message => console.log(message.sid));
     // console.log(req);
     // return res.status(200).send();
